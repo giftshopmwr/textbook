dfdce45760b689cd407c27e3350123a459784374
shosetim <shosetim32@gmail.com>
2024-03-19 16:31:16 +0800

    js 路徑更新

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const navbar = document.querySelector('[data-navbar]');
-
 const navText = `
 <li class="nav-item dropdown">
   <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
